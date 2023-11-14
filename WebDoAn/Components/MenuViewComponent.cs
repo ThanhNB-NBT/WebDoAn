e304906f98420775b133ec509a81bd9eb11d2647
thanhpe <nguyenthanh120223@gmail.com>
2023-11-14 22:39:24 +0700

    Add project files.

---
+++ WebDoAn/Components/MenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDoAn.Models;
+
+namespace WebDoAn.Components
+{
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly DataContext _context;
+        public MenuViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofMenu = (from m in _context.Menus
+                              where (m.IsActive == true) && (m.Position == 1)
+                              select m).ToList();
+            //m.Position == 1 là những menu nằm phía trên
+            //m.Position == 2 là những menu nằm phía dưới
+            return await Task.FromResult((IViewComponentResult)View("Default", listofMenu));
+        }
+    }
+}
---
+++ WebDoAn/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+using System.Collections.Generic;
+
+namespace WebDoAn.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
